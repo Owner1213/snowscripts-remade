6dc017491c9947f06eb4dfc1f509baa1af9baf8e
snoww <137589536+Owner1213@users.noreply.github.com>
2025-03-23 18:19:24 -0400

    Rename loadstring.lua to Bootstrapper.lua

---
+++ Bootstrapper.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Owner1213/snowscripts-remade/main/NewMainScript.lua", true))()
--- loadstring.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Owner1213/snowscripts-remade/main/NewMainScript.lua", true))()
