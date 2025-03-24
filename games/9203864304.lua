c791ae20b8dd25e592a55e20458ea440d4dadbd9
snoww <137589536+Owner1213@users.noreply.github.com>
2025-03-24 17:40:23 -0400

    fix

--- games/9203864304.lua
+++ games/9203864304.lua
@@ -228,7 +228,7 @@ run(function()
 
     collectrecipe = chef:CreateToggle({
         Name = 'Collect Recipe',
-        Function = function(val) end
+        Function = function(val) end,
         Tooltip = 'collects the recipe when done cooking'
     })
 end)
