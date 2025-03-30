f4ffe944dc962e6a814b24a08a669b7b34734557
snoww <137589536+Owner1213@users.noreply.github.com>
2025-03-30 11:36:45 -0400

    fix

--- games/17716503056.lua
+++ games/17716503056.lua
@@ -93,7 +93,7 @@ run(function()
     local t = {}
 
     for _, v in pairs(lplr.PlayerGui:WaitForChild('Market').CropsList:GetChildren()) do
-        if v:IsA('TextButton') then 
+        if v:IsA('TextButton') and v.Name ~= 'Template' then 
             table.insert(t, v.Name)
         end
     end
